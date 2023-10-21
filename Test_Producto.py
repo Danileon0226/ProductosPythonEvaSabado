d8df28de6ea4cfb50269f790826cda1b4282cfe0
DaniLeon <122113337+Danileon0226@users.noreply.github.com>
2023-10-21 15:45:09 -0500

    Commit Class Producto
    se creo el Test y la clase Producto

---
+++ Test_Producto.py
@@ -0,0 +1,12 @@
+from main import Producto
+
+producto1 = Producto(id=None,
+                     nombre=None,
+                     descripcion=None,
+                     costo=None,
+                     cantidad=None,
+                     margen_de_venta=None)
+
+producto1.registrar()
+producto1.calcular_precio_venta()
+producto1.ver_registro()
